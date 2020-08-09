d86e35f55d482501af198588e20cc4c99bfdab07
xiawp <307062817@qq.com>
2020-08-09 23:19:26 +0800

    tijiao

---
+++ config-server/src/main/java/com/zantong/bank/ConfigApplication.java
@@ -0,0 +1,22 @@
+package com.zantong.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @program: bank
+ * @description:
+ * @author: xwp
+ * @create: 2020-08-09 22:23
+ **/
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
